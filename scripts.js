dd48866e737da65e9775bfd1b21a05dcef98350c
andre-0303 <bandeiradev.ti@gmail.com>
2025-04-25 07:42:20 -0300

    feat: correcao scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
+console.log("JS carregado com sucesso!");
+
 fetch('https://gutendex.com/books/')
   .then(response => response.json()) 
   .then(data => {
+    console.log(data); 
     const lista = document.getElementById('lista-livros'); 
 
     data.results.slice(0, 5).forEach(livro => {
       const item = document.createElement('li'); 
-
       item.textContent = `${livro.title} - ${livro.authors[0]?.name ?? 'Autor desconhecido'}`;
-
       lista.appendChild(item);
     });
   })
-  .catch(error => console.error('Erro ao buscar livros:', error)); 
+  .catch(error => console.error('Erro ao buscar livros:', error));
